9860952202ae4b006f967035b0e0a86a98ff439d
gregsadetsky <lepetitg@gmail.com>
2024-03-03 15:22:48 -0500

    fix name after rename

--- actamap/urls.py
+++ actamap/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""actamap URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from mainmap.views import HomePageView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from mainmap.views import HomePageView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mainmap.urls')),
-    path('', HomePageView.as_view()),
+    path("admin/", admin.site.urls),
+    path("", include("mainmap.urls")),
+    path("", HomePageView.as_view()),
 ]
