dd7d47b3952cbedfb8c154ce604ccba68bdb60f3
gregsadetsky <lepetitg@gmail.com>
2024-03-03 16:01:51 -0500

    more tailwind styles

--- bin/tailwind-watch.sh
+++ bin/tailwind-watch.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIRECTORY="$(dirname "$0")"
 
-npx tailwindcss -c $SCRIPT_DIRECTORY/../tailwind.config.js \
+npx tailwindcss \
+  -c $SCRIPT_DIRECTORY/../tailwind.config.js \
   -i $SCRIPT_DIRECTORY/../mainmap/static/mainmap/css/base.css \
   -o $SCRIPT_DIRECTORY/../mainmap/static/mainmap/css/tailwind-out.css --watch
